5c226c05aeda77d2a69cab87e4d738e60547d407
Ryan Rickgauer <rrickgauer1@gmail.com>
2020-11-24 14:45:55 -0600

    cancel a note edit

--- src/js/notebook.js
+++ src/js/notebook.js
@@ -16,8 +16,11 @@ function addListeners() {
 
 
   $('.pages').on('click', '.btn-page-edit', function(e) {
-    $(this).closest('.card-page').find('.content').removeClass('display-mode-normal');
-    $(this).closest('.card-page').find('.content').addClass('display-mode-edit');
+    togglePageDisplayMode(this);
+  });
+
+  $('.pages').on('click', '.card-page .btn-page-update-cancel', function(e) {
+    togglePageDisplayMode(this);
   });
 
   $('.pages').on('click', '.card-page .btn-page-update-save', function(e) {
@@ -25,33 +28,6 @@ function addListeners() {
   });
 
 }
-
-
-
-function updateNoteContent(selector) {
-  const note = $(selector).closest('.card-page');
-  const noteID = $(note).attr('data-page-id');
-  const newContent = $(note).find('.edit-input').val();
-
-
-  const data = {
-    function: constants.API_FUNCTIONS.updateNote,
-    noteID: noteID,
-    content: newContent,
-  }
-
-  $.post(constants.API, data, function(response) {
-    console.log(response);
-  });
-
-
-
-}
-
-
-
-
-
 
 
 ///////////////////////////////////////////////////////////////
@@ -64,7 +40,9 @@ function enableNewPageBtn() {
     $('.btn-page-new-create').prop('disabled', true);
 }
 
-
+////////////////////////
+// Inserts a new page //
+////////////////////////
 function insertPage() {
   const name = $('#page-new-name').val();
   const type = $('input[name="page-new-type"]:checked').val();
@@ -80,29 +58,37 @@ function insertPage() {
   });
 }
 
+
+///////////////////////////////////////
+// Loads all pages from the database //
+///////////////////////////////////////
 function loadPages() {
   const data = {
     function: constants.API_FUNCTIONS.getPages,
     notebookID: globalVariables.notebookID,
   }
 
-
   $.getJSON(constants.API, data, function(response) {
-    for (let count = 0; count < response.length; count++)
+    for (let count = 0; count < response.length; count++) {
       addPage(response[count]);
+    }
 
     displayPages();
-    // console.log($('.card-page').html());
   });
 }
 
+//////////////////////////////////////////////
+// Insert a new page into the list of pages //
+//////////////////////////////////////////////
 function addPage(page) {
   const newPage = new Page(page);
   pagesList.push(newPage);
 }
 
+/////////////////////////////////////////////////////////
+// Runs down the list of pages and displays their html //
+/////////////////////////////////////////////////////////
 function displayPages() {
-  
   let html = '';
 
   for (let count = 0; count < pagesList.length; count++) {
@@ -112,9 +98,32 @@ function displayPages() {
   $('.pages').html(html);
 }
 
+/////////////////////////////////////////////////////
+// Toggles a page's display mode to edit or normal //
+/////////////////////////////////////////////////////
+function togglePageDisplayMode(selector) {
+  $(selector).closest('.card-page').find('.content').toggleClass('display-mode-normal');
+  $(selector).closest('.card-page').find('.content').toggleClass('display-mode-edit');
+}
 
+/////////////////////////////
+// Update a note's content //
+/////////////////////////////
+function updateNoteContent(selector) {
+  const note = $(selector).closest('.card-page');
+  const noteID = $(note).attr('data-page-id');
+  const newContent = $(note).find('.edit-input').val();
 
+  const data = {
+    function: constants.API_FUNCTIONS.updateNote,
+    noteID: noteID,
+    content: newContent,
+  }
 
+  $.post(constants.API, data, function(response) {
+    console.log(response);
+  });
+}
 
 
 
