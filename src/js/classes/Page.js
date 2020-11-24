0bb4444a308abefb2e1c9e2c9550ed489da4bd45
Ryan Rickgauer <rrickgauer1@gmail.com>
2020-11-24 13:51:05 -0600

    break down get html into smaller functions

--- src/js/classes/Page.js
+++ src/js/classes/Page.js
@@ -21,52 +21,66 @@ function Page(parms) {
 
 
 Page.prototype.getHtml = function() {
+  let html = '';
+  html += `<div class="card card-page" data-page-id="${this.id}">`;
+  html += this.getHtmlHeader();
+  html += this.getHtmlBody();
+  html += '</div>';   // end card
+  return html;
+}
 
+Page.prototype.getHtmlHeader = function() {
   let html = `
-  <div class="card card-page" data-page-id="${this.id}">
-    <div class="card-header">
-      <div class="left">
-        <h5 class="card-page-name">${this.name}</h5>
-        <p>&nbsp;&bull;&nbsp;<span class="card-page-date-created">${this.dateCreatedDisplay}</span></p>
-      </div>
-
-      <div class="right">
-        <div class="dropdown">
-          <button class="btn btn-sm" type="button" data-toggle="dropdown">
-            <i class='bx bx-dots-horizontal'></i>
-          </button>
-          <div class="dropdown-menu dropdown-menu-right">
-            <button class="dropdown-item btn-page-edit" type="button">Edit</button>
-            <button class="dropdown-item btn-page-hide" type="button">Hide</button>
-            <button class="dropdown-item btn-page-delete" type="button">Delete</button>
-          </div>
-        </div>
-      </div>          
+  <div class="card-header">
+    <div class="left">
+      <h5 class="card-page-name">${this.name}</h5>
+      <p>&nbsp;&bull;&nbsp;<span class="card-page-date-created">${this.dateCreatedDisplay}</span></p>
     </div>
 
-    <div class="card-body">
-      <div class="content display-mode-normal">
-
-        <div class="rendered">
-          ${this.content}
+    <div class="right">
+      <div class="dropdown">
+        <button class="btn btn-sm" type="button" data-toggle="dropdown">
+          <i class='bx bx-dots-horizontal'></i>
+        </button>
+        <div class="dropdown-menu dropdown-menu-right">
+          <button class="dropdown-item btn-page-edit" type="button">Edit</button>
+          <button class="dropdown-item btn-page-hide" type="button">Hide</button>
+          <button class="dropdown-item btn-page-delete" type="button">Delete</button>
         </div>
-
-        <div class="edit">
-          <nav>
-            <div class="nav nav-tabs" role="tablist">
-              <button class="nav-link write active" data-toggle="tab" data-target=".card-page[data-page-id='${this.id}'] .tab-pane.write" role="tab">Write</button>
-              <button class="nav-link preview" role="tab" data-toggle="tab" data-target=".card-page[data-page-id='${this.id}'] .tab-pane.preview">Preview</button>
-            </div>
-          </nav>
-          <div class="tab-content">
-            <div class="tab-pane write fade show active" role="tabpanel">
-              write
-            </div>
-            <div class="tab-pane preview fade" role="tabpanel">preview</div>
-          </div>
-        </div>
-
       </div>
+    </div>          
+  </div>`;
+
+  return html;
+}
+
+
+Page.prototype.getHtmlBody = function() {
+  const dataTarget = `.card-page[data-page-id='${this.id}'] .tab-pane`;
+
+  let html = `
+  <div class="card-body">
+    <div class="content display-mode-normal">
+
+      <div class="rendered">
+        ${this.content}
+      </div>
+
+      <div class="edit">
+        <nav>
+          <div class="nav nav-tabs" role="tablist">
+            <button class="nav-link active" data-toggle="tab" data-target="${dataTarget}.write">Write</button>
+            <button class="nav-link" data-toggle="tab" data-target="${dataTarget}.preview">Preview</button>
+          </div>
+        </nav>
+        <div class="tab-content">
+          <div class="tab-pane write show active" role="tabpanel">
+            write
+          </div>
+          <div class="tab-pane preview" role="tabpanel">preview</div>
+        </div>
+      </div>
+
     </div>
   </div>`;
 
--- src/notebook.php
+++ src/notebook.php
@@ -40,7 +40,57 @@ $notebook = DB::getNotebook($_GET['notebookID'])->fetch(PDO::FETCH_ASSOC);
     <h4 class="my-4">Your pages</h4>
 
     <!-- pages -->
-    <div class="pages"></div>
+    <div class="pages">
+      
+
+      <div class="card card-page" data-page-id="${this.id}">
+        <div class="card-header">
+          <div class="left">
+            <h5 class="card-page-name">${this.name}</h5>
+            <p>&nbsp;&bull;&nbsp;<span class="card-page-date-created">${this.dateCreatedDisplay}</span></p>
+          </div>
+
+          <div class="right">
+            <div class="dropdown">
+              <button class="btn btn-sm" type="button" data-toggle="dropdown">
+                <i class='bx bx-dots-horizontal'></i>
+              </button>
+              <div class="dropdown-menu dropdown-menu-right">
+                <button class="dropdown-item btn-page-edit" type="button">Edit</button>
+                <button class="dropdown-item btn-page-hide" type="button">Hide</button>
+                <button class="dropdown-item btn-page-delete" type="button">Delete</button>
+              </div>
+            </div>
+          </div>          
+        </div>
+
+        <div class="card-body">
+          <div class="content display-mode-normal">
+
+            <div class="rendered">
+              ${this.content}
+            </div>
+
+            <div class="edit">
+              <nav>
+                <div class="nav nav-tabs" role="tablist">
+                  <button class="nav-link write active" data-toggle="tab" data-target=".card-page[data-page-id='${this.id}'] .tab-pane.write" role="tab">Write</button>
+                  <button class="nav-link preview" role="tab" data-toggle="tab" data-target=".card-page[data-page-id='${this.id}'] .tab-pane.preview">Preview</button>
+                </div>
+              </nav>
+              <div class="tab-content">
+                <div class="tab-pane write fade show active" role="tabpanel">
+                  write
+                </div>
+                <div class="tab-pane preview fade" role="tabpanel">preview</div>
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </div>
+
+    </div>
 
 
 
